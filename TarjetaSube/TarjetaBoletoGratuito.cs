0c3778476ac0cff142f0dbb84a7fe4911ab8e656
Franco Rubin <rubinfranco07@gmail.com>
2025-10-28 09:30:14 -0300

    subo las cosas a la rama (#4)

---
+++ TarjetaSube/TarjetaBoletoGratuito.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TarjetaSube
+{
+    public class TarjetaBoletoGratuito : Tarjeta
+    {
+        public TarjetaBoletoGratuito() : base()
+        {
+        }
+
+        public override bool DescontarSaldo(decimal monto)
+        {
+        
+            return true;
+        }
+
+        public override decimal ObtenerTarifa(decimal tarifaBase)
+        {
+            return 0;
+        }
+    }
+}
---
+++ TarjetaSube/TarjetaFranquiciaCompleta.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TarjetaSube
+{
+    public class TarjetaFranquiciaCompleta : Tarjeta
+    {
+        public TarjetaFranquiciaCompleta() : base()
+        {
+        }
+
+        public override bool DescontarSaldo(decimal monto)
+        {
+        
+            return true;
+        }
+
+        public override decimal ObtenerTarifa(decimal tarifaBase)
+        {
+            return 0;
+        }
+    }
+}
