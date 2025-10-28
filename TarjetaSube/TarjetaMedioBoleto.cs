f21d1c8ea84b0416eb79808957e05aa56f5bd781
Franco Rubin <rubinfranco07@gmail.com>
2025-10-28 09:28:41 -0300

    subo las cosas a la rama

---
+++ TarjetaSube/TarjetaMedioBoleto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TarjetaSube
+{
+    public class TarjetaMedioBoleto : Tarjeta
+    {
+        public TarjetaMedioBoleto() : base()
+        {
+        }
+
+        public override decimal ObtenerTarifa(decimal tarifaBase)
+        {
+            return tarifaBase / 2;
+        }
+    }
+}
